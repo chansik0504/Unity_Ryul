2be376b8daf091464603bdf5b03ecca1d6fa5e69
chansik0504 <chansik0504@gmail.com>
2022-05-18 18:11:16 +0900

    UI_UX

---
+++ Assets(UI_UX)/02. Scripts/ItemControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemControl : MonoBehaviour
+{
+    public GameObject Item_text;
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "player")
+        {
+            Item_text.GetComponent<Text>().text = "아이템을 주우려면 [Z]키를 눌러주세요";
+
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                Item_text.GetComponent<Text>().text = "";
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "player")
+        {
+            Item_text.GetComponent<Text>().text = "";
+        }
+    }
+}
---
+++ Assets(UI_UX)/02. Scripts/PlayerControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    float dirX = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxis("Horizontal");
+        Vector3 moveDir = new Vector3(dirX, 0, 0);
+        transform.Translate(moveDir * Time.deltaTime);
+    }
+}
