f27e1ab74f0d0b35cc6062dd74c43085127fc4a4
chansik0504 <chansik0504@gmail.com>
2022-05-18 18:10:22 +0900

    DressUp

---
+++ Assets(DressUp)/02. Scripts/exList.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exList : MonoBehaviour
+{
+    public GameObject[] enemies;
+    List<GameObject> newList = new List<GameObject>();
+
+    void Start()
+    {
+        foreach (GameObject e in enemies)
+        {
+            if (e.tag == "Enemy")
+            {
+                newList.Add(e);
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+        for (int i = 0 ; i < newList.Count; i++)
+        {
+            newList[i].GetComponent<Renderer>().material.color = Color.red;
+            Debug.Log(newList[i].name);
+        }
+    }
+}
