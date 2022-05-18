93c811ac1c2fbeed84568fd390b2fa8b1ddb7982
chansik0504 <chansik0504@gmail.com>
2022-05-18 18:12:31 +0900

    템플런

---
+++ Assets(템플런)/02. Scripts/csBridge.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csBridge : MonoBehaviour
+{
+    public GameObject[] bridges; //다리 - 프리팹
+    public GameObject[] coins; //동전 - 프리팹
+    public GameObject bridgeTurn; //교차로
+    //다리 전체를 담는 부모 오프젝트다. 자신외에 10개의 다리가 child로 설정될것이다.
+    GameObject newBridge; //새로만들다리 - parent
+    
+    //각각의 다리(또는 동전)로 newBridge의 하위 오브젝트이다.
+    GameObject childBridge; //각각의다리(동전) - child
+    //Player가 교차로에서 회전하면 그 방향으로 다리를 다시 만들고 예전의 다리는 삭제한다.
+    GameObject oldBridge; //삭제할 예전의 다리
+    //다리의종류(다리1~5)를 설정할 변수이다.
+    GameObject bridge; //다리종류 - 프리팹 만들기용
+    GameObject coin; //동전셋 종류 - 프리팹 만들기용
+    bool canCoin = false; //동전만들기
+    int dir = 0; //주인공의 회전방향(0~3)
+    Quaternion quatAng; //주인공의 회전각
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newBridge = GameObject.Find ("StartBridge");
+        oldBridge = GameObject.Find ("oldBridge");
+        childBridge = newBridge; //자식의 위치를 부모와 일치시킴
+        MakeBridge ("FORWARD"); //전방으로 다리만들기
+    }
+
+    void MakeBridge(string sDir)
+    {
+        DeleteOldBridge(); //Player가 지나간다리
+        CalcRotation(sDir); //주인공의 회전각 계산
+        MakeNewBridge(); //회전방향으로 다리 만들기
+    }
+
+    void DeleteOldBridge()
+    {
+        Destroy (oldBridge); //예전 다리 삭제
+        oldBridge = newBridge; //현재의 다리 저장
+        //다리 시작점 만들기
+        newBridge = new GameObject ("StartBridge"); //스테이지에 있는 StartBridge를 새로
+        // 만들어서 newBridge에 저장한다.
+    }
+
+//Player의 회전방향과 각도계산
+    void CalcRotation(string sDir)
+    {
+        switch (sDir) 
+        {
+            case "LEFT":
+            dir--; //왼쪽으로 이동
+            break;
+            case "RIGHT":
+            dir++; //오른쪽으로 이동
+            break;
+        }
+        //회전방향을 0~3으로 제한
+        if (dir < 0) dir = 3;
+        if (dir > 3) dir = 0;
+        //회전각을 쿼터니언으로 변환
+        quatAng.eulerAngles = new Vector3 (0, dir * 90, 0);
+    }
+
+
+    void MakeNewBridge()
+    {
+        for (int i = 0; i < 10; i++) 
+        {
+            bridge = bridges [0]; //기본다리 (다리의 시작점은 기본다리1이다)
+            coin = coins [Random.Range (0, 3)]; //동전셋 (동전셋은 랜덤하게 설정한다)
+            canCoin = false;
+
+            //Debug.Log(i);
+            SelectBridge(i); //다리종류설정 (랜덤한 종류의 다리가 나오도록 한다)
+
+            Vector3 pos = Vector3.zero; //다리의 position (Instantiate() 함수에서 다리의 위치를 지정할 변수)
+            Vector3 localPos = childBridge.transform.localPosition; //다리의 localPosition (맨
+            // 마지막으로 만들어진 다리의 localPosition으로 이 위치를 기준으로 새로운 다리를 만든다)
+            switch (dir) 
+            {//주인공의 회전방향에 따라 각각의 위치에 다리를 만든다.
+                case 0:
+                pos = new Vector3 (localPos.x, 0, localPos.z + 10);
+                break;
+                case 1:
+                pos = new Vector3 (localPos.x + 10, 0, localPos.z);
+                break;
+                case 2:
+                pos = new Vector3 (localPos.x, 0, localPos.z - 10);
+                break;
+                case 3:
+                pos = new Vector3 (localPos.x - 10, 0, localPos.z);
+                break;
+            }
+            Debug.Log(bridge);
+            //새로운 다리를 만들고 부모설정
+            childBridge = Instantiate (bridge, pos, quatAng) as GameObject;
+            childBridge.transform.parent = newBridge.transform; //(새로 만든 다리의 부모를 설정한다)
+
+            if (canCoin) 
+            {
+                childBridge = Instantiate (coin, pos, quatAng) as GameObject;
+                childBridge.transform.parent = newBridge.transform;
+            }
+        }
+    }
+
+    void SelectBridge(int n)
+    {
+        switch (n) 
+        {
+            case 9: //마지막 다리는 교차로
+                bridge = bridgeTurn;
+                break;
+            case 1: //홀수번째 다리는 장애물이 있는 다리
+            case 3:
+            case 5:
+            case 7:
+                bridge = bridges [Random.Range (0, bridges.Length)];
+                break;
+            default: //짝수번째 다리에는 동전을 만든다.
+                if (Random.Range (0, 100) > 50) 
+                {
+                    canCoin = true;
+                }
+                break;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets(템플런)/02. Scripts/csPlayer.cs
@@ -0,0 +1,249 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class csPlayer : MonoBehaviour
+{
+    GameObject manager; //Bridge Manager
+    Animation anim;
+    int speedForward = 10; //전진속도
+    int speedSide = 6; //옆걸음 속도
+    int jumpPower = 300; //점프
+    bool canJump = true; //점프가능
+    bool canTurn = false; //회전가능
+    bool canLeft = true; //왼쪽이동가능
+    bool canRight = true; //오른쪽이동가능
+    bool isGround = true; //바닥에 있는지?
+    bool isDead = false; //죽었나?
+    float dirX = 0; //좌우이동방향 > -1:왼쪽 1:오른쪽
+    float score = 0;
+    Vector3 touchStart; //모바일기기의 Touch 시작위치
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        manager = GameObject.Find("BridgeManager");
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == true)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        CheckMove(); //이동 및 점프가능여부 체크
+        MoveHuman(); //주인공 이동
+
+        score += Time.deltaTime * 1000;//득점처리
+    }
+
+    void CheckMove()
+    {
+        RaycastHit hit;
+        //디버그용
+        Debug.DrawRay(transform.position, Vector3.down * 2f, Color.red);
+        Debug.DrawRay(transform.position, Vector3.left * 0.7f, Color.red);
+        Debug.DrawRay(transform.position, Vector3.right * 0.7f, Color.red);
+        /*
+        레이케스트 사용법
+        Physics.Raycast(기준점, 방향, hitInfo, 거리) > 가장 가까운 물체탐색 
+        */
+        isGround = true;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 2f))
+        {
+            if (hit.transform.tag == "BRIDGE")
+                isGround = true;
+        }
+        canLeft = true;
+
+        if (Physics.Raycast(transform.position, Vector3.left, out hit, 0.7f))
+        {
+            if (hit.transform.tag == "GUARD")
+                canLeft = false;
+        }
+        canRight = true;
+
+        if (Physics.Raycast(transform.position, Vector3.right, out hit, 0.7f))
+        {
+            if (hit.transform.tag == "GUARD")
+                canRight = false;
+        }
+    }
+
+    void MoveHuman()
+    {
+        dirX = 0;
+
+        if (Application.platform == RuntimePlatform.Android ||
+        Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            CheckMobile();
+        }
+
+        else
+        {
+            CheckKeyboard();
+        }
+
+        Vector3 moveDir = new Vector3(dirX * speedSide, 0, speedForward);
+        transform.Translate(moveDir * Time.deltaTime);
+    }
+
+    void CheckKeyboard()
+    {
+        if (isGround)
+        {
+            dirX = Input.GetAxis("Horizontal");
+            if (canJump && Input.GetKeyDown(KeyCode.Space))
+            {
+                StartCoroutine("JumpHuman");
+            }
+        }
+        if (canTurn)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+                RotateHuman("LEFT");
+            if (Input.GetKeyDown(KeyCode.E))
+                RotateHuman("RIGHT");
+        }
+    }
+
+    IEnumerator JumpHuman()
+    {
+        canJump = false;
+        gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpPower);
+        //GetComponent<Animator>().SetInteger("Jump0", 1);//메카님방식의 에니메이션일때
+        anim.Play("jump_pose");
+        yield return new WaitForSeconds(1);
+        //GetComponent<Animator>().SetInteger("Jump0", 0);//메카님방식의 에니메이션일때
+        anim.Play("run");
+        canJump = true;
+    }
+
+    //충돌 - DeadZone
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.tag == "DEAD")
+        {
+            isDead = true;
+            anim.Play("idle");
+        }
+    }
+    //충돌시작 – 기타
+    void OnTriggerEnter(Collider col)
+    {
+        switch (col.transform.tag)
+        {
+            case "TURN":
+                canTurn = true;
+                break;
+            case "COIN":
+                score += 1000;
+                Destroy(col.gameObject);
+                break;
+        }
+    }
+    //충돌끝
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "TURN")
+        {
+            canTurn = false;
+        }
+    }
+
+    void RotateHuman(string sDir)
+    {
+        canTurn = false; //반복회전금지
+        Vector3 rot = transform.eulerAngles;//현재의 회전각 구하기
+
+        switch (sDir)
+        {
+            case "LEFT":
+                rot.y -= 90;
+                break;
+            case "RIGHT":
+                rot.y += 90;
+                break;
+        }
+
+        transform.eulerAngles = rot;
+        //주인공 방향으로 다리 만들기 - 메세지보내기로 함수호출
+        manager.SendMessage("MakeBridge", sDir, SendMessageOptions.DontRequireReceiver);
+    }
+
+
+    void OnGUI()
+    {
+        string str = "<size=20><color=#000000>score: ##</color></size>";
+        GUI.Label(new Rect(10, 10, 300, 80), str.Replace("##", "" + (int)score));
+
+        if (!isDead)
+            return;
+
+        int w = Screen.width / 2;
+        int h = Screen.height / 2;
+
+        if (GUI.Button(new Rect(w - 60, h - 50, 120, 50), "Play Game"))
+        {
+            SceneManager.LoadScene("Main");
+            // Application.LoadLevel("Main");
+        }
+
+        if (GUI.Button(new Rect(w - 60, h + 50, 120, 50), "Quit Game"))
+        {
+            Application.Quit();
+        }
+    }
+
+
+    void CheckMobile()
+    {
+        float x = Input.acceleration.x;
+
+        if (canLeft && isGround)
+        {
+            if (x < -0.2f)
+            dirX = -0.6f;
+            else if (x > 0.2f)
+            dirX = 0.6f;
+        }
+
+        foreach (Touch tmp in Input.touches)
+        {
+            if (tmp.phase == TouchPhase.Began)
+            {
+                touchStart = tmp.position;
+            }
+            if (tmp.phase == TouchPhase.Moved)
+            {
+                Vector3 touchEnd = tmp.position;
+
+                if (isGround && canJump && touchEnd.y - touchStart.y > 100)
+                {
+                    StartCoroutine("JumpHuman");
+                }
+
+                if (canTurn && touchEnd.x - touchStart.x < -100)
+                {
+                    RotateHuman("LEFT");
+                }
+
+                if (canTurn && touchEnd.x - touchStart.x > 100)
+                {
+                    RotateHuman("RIGHT");
+                }
+            }
+        }
+    }
+}
